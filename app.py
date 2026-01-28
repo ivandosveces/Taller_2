da3aea8ba880d9e5c93a64c6046dae508d6c6adb
Ivan Eslava <j.eslavag@uniandes.edu.co>
2026-01-27 21:18:07 -0500

    Actualización después de publicar en GitHub

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia - rama ivan-viz"
+app.title = "Dashboard energia - version publicada"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
